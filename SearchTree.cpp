3641dc1ff486afba50ef08f664b69c55994bd4a6
fairuzathrz <fayizpdp2008@gmai.com>
2026-06-11 15:50:20 +0700

    Menghubungkan Search ke dalam Fungsi Insert

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -2,44 +2,8 @@
 #include <string>
 using namespace std;
 
-class Node
-{
-public:
-    string info;
-    Node *leftchild;
-    Node *rightchild;
 
-    // Constructor for the node class
-    Node(string i, Node *l, Node *r)
-    {
-        info = i;
-        leftchild = l;
-        rightchild = r;
-    }
-};
 
-class BinaryTree
-{
-public: 
-    Node *ROOT;
-
-    BinaryTree()
-    {
-        ROOT = NULL; // Initializing ROOT to nul
-    }
-
-    // Insert a node in the binary search tree
-    void insert(string element)
-    {
-        Node *newNode = new Node(element, NULL, NULL);
-
-        newNode->info = element;
-        newNode->leftchild = NULL;
-        newNode->rightchild = NULL;
-
-        Node *parent = NULL;
-        Node *currentNode = NULL;
-    }
-};
+        search(element, parent, currentNode);
 
        
