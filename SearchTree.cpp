8a24ce24daeb5867df7f7c245d875dd53d67e950
fairuzathrz <fayizpdp2008@gmai.com>
2026-06-11 15:54:42 +0700

    Menambahkan Fungsi Postorder Traversal

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -2,21 +2,23 @@
 #include <string>
 using namespace std;
 
-
-    void preorder(Node *ptr)
+{
+    void postorder(Node *ptr)
     {
+        // perfoms the postorder traveersal of the tree
+
         if (ROOT == NULL)
         {
-            cout <<"Tree is empty" <<  endl;
+            cout << "Tree is empty" << endl;
             return;
         }
 
         if (ptr != NULL)
         {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
             cout << ptr->info << " ";
-            preorder(ptr->leftchild);
-            preorder(ptr->rightchild);
         }
     }
+};
 
-  
