2b5a91d11a29bf3a8a0f624571a9d307c4f58e32
fairuzathrz <fayizpdp2008@gmai.com>
2026-06-11 15:53:40 +0700

    Menambahkan Fungsi Preorder Traversal

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -2,20 +2,21 @@
 #include <string>
 using namespace std;
 
-    void inorder(Node *ptr)
+
+    void preorder(Node *ptr)
     {
         if (ROOT == NULL)
         {
-            cout << "Tree is empty" << endl;
+            cout <<"Tree is empty" <<  endl;
             return;
         }
 
         if (ptr != NULL)
         {
-            inorder(ptr->leftchild);
             cout << ptr->info << " ";
-            inorder(ptr->rightchild);
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
         }
     }
 
-   
+  
